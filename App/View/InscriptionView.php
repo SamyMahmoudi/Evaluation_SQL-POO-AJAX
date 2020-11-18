964424086dd825b4a88205ae681c6988982723a2
Nathan Szigeti <nathan.szigeti@edu.devinci.fr>
2020-11-18 17:35:54 +0100

    nathan1

--- App/View/InscriptionView.php
+++ App/View/InscriptionView.php
@@ -24,21 +24,47 @@
         </ul>
     </nav>
 
-    <div class="main-inscription">
+    <?php 
 
-        <form action="" id="form-inscription">
+        $bdd = new PDO ('mysql:host=localhost;dbname=sondapote', 'root', '');
+
+        if(isset($_POST['valid'])) {
+            
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            
+            $verif = $bdd -> prepare("SELECT COUNT(*) FROM t_user WHERE user_name = ".$_POST['username'].", user_mail = ".$_POST['email']."");
+            $verif->execute(array ());
+            $donnees = $verif->fetchColumn();
+            if ($donnees == 0){
+                if(!empty($_POST['username']) AND !empty($_POST['email']) AND !empty($_POST['mdpasse'])){
+                    if($_POST['mdpasse'] == $_POST['confirm']){
+                        $requete = $bdd->prepare("INSERT INTO t_user(user_name, user_mail, user_password) VALUES(?,?,?)");
+                        $requete->execute(array($_POST["username"], $_POST["email"], $_POST["mdpasse"]));
+                    }
+                    else{
+                        echo 'mot de passe mal écrit';
+                    }
+                }else{
+                    echo 'champs non remplis';
+                }
+            }else{
+                echo 'Mail ou pseudo déjà existant';
+            }
+            var_dump($donnees);
+        }
+    ?>
+
+    <div class="main-inscription">
+        <form method="POST" id="form-inscription" >
 
             <h1>Formulaire d'inscription</h1>
 
-            <input type="text" name="" placeholder="entrez votre nom d'utilisateur">
-
-            <input type="email" name="" placeholder="entrez votre adresse mail">
-
-            <input type="password" name="" placeholder="entrez un mot de passe">
-
-            <input type="password" name="" placeholder="entrez un mot de passe">
-
-            <input type="submit" value="S'inscrire">
+            <input type="text" name="username" placeholder="entrez votre nom d'utilisateur" >
+            <input type="email" name="email" placeholder="entrez votre adresse mail" >
+            <input type="password" name="mdpasse" placeholder="entrez un mot de passe" >
+            <input type="password" name="confirm" placeholder="validez le mot de passe">
+            <input type="submit" name="valid" value="S'inscrire">
 
         </form>
         
