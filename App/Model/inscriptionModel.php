c147c363c5d43f93cb069923b4b4154b1959daa7
Nathan Szigeti <nathan.szigeti@edu.devinci.fr>
2020-11-23 18:31:21 +0100

    creation de sondage

--- App/Model/inscriptionModel.php
+++ App/Model/inscriptionModel.php
@@ -11,8 +11,9 @@ class InscriptionModel extends Database{
         if(isset($_POST['valid'])) {
             
             //Prend les données des input
-            $username = $_POST['username'];
-            $email = $_POST['email'];
+            $username = trim($_POST['username']);
+            $email = trim($_POST['email']);
+            $mdp = password_hash($_POST['mdpasse'], PASSWORD_DEFAULT);
             
             //Fait la verif si pseudo existe dans la BDD
             $verif = $this->pdo->prepare("SELECT user_name FROM t_users WHERE user_name = ?");
@@ -23,25 +24,25 @@ class InscriptionModel extends Database{
                 $verif2 = $this->pdo->prepare("SELECT user_mail FROM t_users WHERE user_mail = ?");
                 $verif2->execute(array($email));
                 $donnees2 = $verif2->rowCount();
-                if($donnees2 == 0){
+                if($donnees2 == 0 AND filter_var($email, FILTER_VALIDATE_EMAIL)){
                 //Verifie si les champs sont remplis
                     if(!empty($_POST['username']) AND !empty($_POST['email']) AND !empty($_POST['mdpasse'])){
                         //Verifie si les mots de passes sont identiques
                         if($_POST['mdpasse'] == $_POST['confirm']){
                             $requete = $this->pdo->prepare("INSERT INTO t_users(user_name, user_mail, user_password) VALUES(?,?,?)");
-                            $requete->execute(array($_POST["username"], $_POST["email"], $_POST["mdpasse"]));
+                            $requete->execute(array($username, $email, $mdp));
                         }
                         else{
-                            echo 'mot de passe mal écrit';
+                            echo '<div id="fond"><div id="erreur"><span>mot de passe mal écrit</span><button id="erreurBouton">OK</button></div>';
                         }
                     }else{
-                        echo 'champs non remplis';
+                        echo '<div id="fond"><div id="erreur"><span>champs non remplis</span><button id="erreurBouton">OK</button></div></div>';
                     }
                 }else{
-                    echo 'Mail déjà existant';
+                    echo '<div id="fond"><div id="erreur"><span>Mail déjà existant</span><button id="erreurBouton">OK</button></div></div';
                 }
             }else{
-                echo 'Pseudo déjà existant';
+                echo '<div id="fond"><div id="erreur"><span>Pseudo déjà existant</span><button id="erreurBouton">OK</button></div></div>';
             }
         }
     }
--- App/View/InscriptionView.php
+++ App/View/InscriptionView.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
     <link rel="stylesheet" href="css/inscription.css">
+    
 </head>
 
 <body class="body-inscription">
@@ -43,5 +44,6 @@
 
 
 </body>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="script/inscription.js"></script>
 </html>
