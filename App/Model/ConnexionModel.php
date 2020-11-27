bf9e8dac59435902939e22699c22b2d943587c4f
Renard Louis-Axel <louisaxelrenard@gmail.com>
2020-11-27 16:24:05 +0100

    connexiondehasher

--- App/Model/ConnexionModel.php
+++ App/Model/ConnexionModel.php
@@ -13,34 +13,45 @@ class ConnexionModel extends Database{
     if(isset($_POST['email']) && isset($_POST['password']))
     {
         $email = htmlspecialchars($_POST['email']);
-        $password = htmlspecialchars($_POST['password']);
-        $check =  $this->pdo->prepare('SELECT * FROM t_users WHERE user_mail = ?');
+
         
-        $check->execute(array($email));
-        $data = $check->fetch();
-        $row = $check->rowCount();
- 
-        if($row == 1)
-        {
-            //if(filter_var($email, FILTER_VALIDATE_EMAIL))
-           // {
-               // $password = hash('sha256', $password);
-                if($data['user_password'] === $password)
-                {   
-                    $_SESSION['userId'] = $data['user_id'];
-                    $_SESSION['userName'] = $data['user_name'];
-                    $_SESSION['connect'] = true;
-
-                    header('Location: index.php?page=profil');
-                    die();
-                }else 
-                header('Location:index.php?page=Connexion&login_err_password');
-                $_SESSION['connect'] = false;
-           // }else header('Location: ConnexionView?login_err=email');
-        }else header('Location:index.php?page=Connexion&login_err_already');
-    }  
-
-    }  
-
-
-}
+        $password = htmlspecialchars($_POST['password']);
+       
+       
+            
+            $check =  $this->pdo->prepare('SELECT * FROM t_users WHERE user_mail = ?');
+            
+            $check->execute(array($email));
+            $data = $check->fetch();
+            $row = $check->rowCount();
+            
+            if($row == 1)
+            {
+               
+                //if(filter_var($email, FILTER_VALIDATE_EMAIL))
+               // {
+                    
+                    
+                    
+                    if(password_verify($password, $data['user_password'] ) )
+                    {   
+                        $_SESSION['userId'] = $data['user_id'];
+                        $_SESSION['userName'] = $data['user_name'];
+                        $_SESSION['connect'] = true;
+    
+                        header('Location: index.php?page=profil');
+                        die();
+                    }else 
+                    
+                    header('Location:index.php?page=Connexion&login_err_password');
+                    $_SESSION['connect'] = false;
+               // }else header('Location: ConnexionView?login_err=email');
+            }else header('Location:index.php?page=Connexion&login_err_already');
+        }  
+    
+        }  
+    
+    
+    }
+       // }
+        
