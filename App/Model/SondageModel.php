161dbcf42089d14d0714b4c5cc0ce511f2253edf
Samy Mahmoudi <samy.mahmoudi@edu.devinci.fr>
2020-12-01 11:15:00 +0100

    update sondages

--- App/Model/SondageModel.php
+++ App/Model/SondageModel.php
@@ -4,12 +4,10 @@ namespace App\Model;
 use Core\Database;
 
 class SondageModel extends Database{
-    
+
     public function recupSondage(){
-        
-        $recherche =$this->pdo->prepare("SELECT * FROM t_users INNER JOIN t_sondages ON t_users.user_id=t_sondages.user_id  WHERE t_sondages.user_id = :user AND t_sondages.sondage_code=:sondage");
+        $recherche =$this->pdo->prepare("SELECT * FROM t_users INNER JOIN t_sondages ON t_users.user_id=t_sondages.user_id  WHERE t_sondages.sondage_code=:sondage");
         $recherche->execute([
-            ":user" => $_SESSION['userId'],
             ":sondage" => $_GET['c']
         ]);
         $foundUser = $recherche->fetchAll(\PDO::FETCH_OBJ);
@@ -17,7 +15,6 @@ class SondageModel extends Database{
     }
 
     public function recupReponse(){
-        
         $recherche =$this->pdo->prepare("SELECT * FROM t_reponses INNER JOIN t_sondages ON t_reponses.sondage_id=t_sondages.sondage_id  WHERE  t_sondages.sondage_code=:sondage");
         $recherche->execute([
             
@@ -40,17 +37,30 @@ class SondageModel extends Database{
     public function repUsers(){
         if(isset($_POST['envoiRep']) AND !empty($_POST['reponse']))
         {
-            $stockRep = $this->pdo->prepare("INSERT INTO t_usersReponses(user_id,sondage_code,reponse_titre) VALUES (?,?,?)");
-            $stockRep->execute(array(
-                $_SESSION['userId'],
-                $_GET['c'],
-                $_POST['reponse'])
-            );
-            $sdg = $this->pdo->prepare("SELECT sondage_id FROM t_sondages WHERE sondage_code = ?");
-            $sdg->execute(array($_GET['c']));
-            $sondage_id = $sdg->fetch(\PDO::FETCH_ASSOC);
-            $choix = $this->pdo->prepare("UPDATE t_reponses SET reponse_score = reponse_score+1 WHERE sondage_id = ? AND reponse_titre = ?");
-            $choix->execute(array($sondage_id['sondage_id'],$_POST['reponse']));
+            $userId = $_SESSION['userId'];
+            $alreadyRep = $this->pdo->prepare("SELECT * FROM t_usersReponses WHERE user_id = ? AND sondage_code = ? ");
+            $alreadyRep->execute(array($userId,$_GET['c']));
+            $row = $alreadyRep->rowCount();
+            if($row == 0){
+                $stockRep = $this->pdo->prepare("INSERT INTO t_usersReponses(user_id,sondage_code,reponse_titre) VALUES (?,?,?)");
+                $stockRep->execute(array(
+                    $_SESSION['userId'],
+                    $_GET['c'],
+                    $_POST['reponse'])
+                );
+                $sdg = $this->pdo->prepare("SELECT sondage_id FROM t_sondages WHERE sondage_code = ?");
+                $sdg->execute(array($_GET['c']));
+                $sondage_id = $sdg->fetch(\PDO::FETCH_ASSOC);
+                $choix = $this->pdo->prepare("UPDATE t_reponses SET reponse_score = reponse_score+1 WHERE sondage_id = ? AND reponse_titre = ?");
+                $choix->execute(array($sondage_id['sondage_id'],$_POST['reponse']));
+            } else {
+                header("location:index.php?page=sondage&answer=yes&c=".$_GET['c']);
+            }
+
         }
+    }
+
+    public function sdgFinish(){    
+        $endate = $this->pdo->query("UPDATE t_sondages SET sondage_statut = 'Finish' WHERE sondage_temps <= NOW()");
     }
 }
